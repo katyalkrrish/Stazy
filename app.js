e42340b32f70bade572f4eea36e8f34ed859ae75
katyalkrrish <katyalkrrish1234@gmail.com>
2025-05-03 22:13:46 +0530

    Add Initial files

---
+++ app.js
@@ -0,0 +1,171 @@
+const express = require("express");
+const app= express();
+const mongoose = require("mongoose");
+const listing = require("../majorproject/models/listing.js")
+const path = require("path");
+const methodOverride = require('method-override');
+const ejsmate= require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
+
+const { listingSchema } = require("./schema.js");
+
+
+app.engine('ejs',ejsmate);
+
+app.use(methodOverride('_method'));
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname,"/public")));
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+
+const validateListing = (req,res,next)=>{
+    let {error} = listingSchema.validate(req.body);
+    // console.log(result);
+    
+    if(error){
+        let errorm = error.details.map(el=>el.message).join(",");
+        throw new ExpressError(400,errorm);
+    }else{
+        next();
+    }
+}
+
+main()
+    .then(()=>{
+    console.log("connection formed");
+    
+}).catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb+srv://katyalkrrish:PjLWZtC93IKNZO3y@cluster0.irbeqgw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+    };
+
+app.get("/",(req,res)=>{
+res.send("hello bhai root bhi chal gya");
+});
+
+
+// app.get("/testListing", async(req,res)=>{
+// let sampleListing= new listing({
+//     title :"My Home",
+//     description: " by the beach",
+//     price:1200,
+//     location:" calangute ,Goa",
+//     country:"India"
+
+
+
+// });
+// await sampleListing.save();
+// console.log("sample was success");
+// res.send("successful testing");
+
+
+// index route
+app.get("/listings",wrapAsync(async (req,res)=>{
+
+      const allistings = await listing.find({});
+        res.render("listings/index.ejs",{allistings});
+}));
+
+
+// new route
+app.get("/listing/new",(req,res)=>{
+    res.render("listings/new.ejs")
+});
+
+//show route
+app.get("/listing/:id",wrapAsync( async (req,res)=>{
+    // console.log("hello1");
+    
+    let {id}= req.params;
+    if(id.length<24){
+        throw new ExpressError(400,"send valid data for listing");
+    }
+    // console.log("hello2");
+   const listings = await listing.findById(id);
+//    console.log(listings);
+
+    res.render("listings/show.ejs",{listings});
+}));
+
+
+// Create route
+
+app.post("/listings",validateListing,wrapAsync( async(req,res,next)=>{
+   
+   
+
+
+// console.log(newlisting);
+const newlisting =new listing(req.body.listing);
+// console.log(newlisting);
+await newlisting.save();
+res.redirect("/listings");
+   
+
+}));
+
+//edit route
+app.get("/listings/:id/edit" , wrapAsync(async(req,res)=>{
+    // console.log("inside edit rout");
+    
+    let {id}= req.params;
+    // console.log("inside edit rout2");
+    const listings = await listing.findById(id);
+    // console.log("inside edit rout3");
+    res.render("listings/edit.ejs",{listings});
+    
+  
+
+}));
+
+
+
+//update route
+
+app.put("/listing/:id",validateListing,wrapAsync(async(req,res)=>{
+    
+    let {id}= req.params;
+    if(!req.body.listing){
+        throw new ExpressError(400,"send valid data for listing");
+    }
+    
+   await listing.findByIdAndUpdate(id,{...req.body.listing});
+    res.redirect(`/listing/${id}`);
+}));
+
+app.delete("/listing/:id",wrapAsync(async (req,res)=>{
+    
+    let {id}= req.params;
+    
+   let deleted= await listing.findByIdAndDelete(id);
+   console.log(deleted);
+   res.redirect("/listings");
+   
+
+}));
+
+app.all("/listings/*",(req,res,next)=>{
+    next(new ExpressError(404,"Page Not Found"));
+});
+
+app.use((err,req,res,next)=>{
+    let {StatusCode=500,message}=err;
+res.status(StatusCode).render("error.ejs",{StatusCode,message});
+    // res.status(StatusCode).send(message);
+});
+
+
+
+
+
+
+
+
+app.listen(3000,()=>{
+    console.log("hello bhai chal gya");
+    console.log("http://localhost:3000/listings")
+})
---
+++ models/listing.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description : String,
+    image: {
+        filename:String,
+     url:{
+        type:String,
+        set: (v) => v === "" ? "https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?q=80&w=1169&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v,
+        default: "https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?q=80&w=1169&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+     }
+    },
+    price: Number,
+    location: String,
+    country: String,
+    reviews:[
+        {
+            type:Schema.Types.ObjectId,
+            ref:"Review",
+        }
+    ]
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
